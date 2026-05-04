f52250ca0ecbf42245fb2711faa4568c8936d4ee
GrMahtb <gregorb.mair@gmail.com>
2026-05-04 15:45:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const BASE  = '/Ankerpruefung/';
-const CACHE = 'htb-ankerpruefung-v10';
+const CACHE = 'htb-ankerpruefung-v11';
 
 // Alle Assets die offline verfügbar sein sollen
 const ASSETS = [
